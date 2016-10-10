6ddb9e4138b7714edbb3d14e81dbcbfb8963959a
bchuup <bchuup@gmail.com>
2016-10-10 21:41:51 +0000

    initial commit

---
+++ main.rb
@@ -0,0 +1,22 @@
+class Game
+  attr_accessor :player1, :player2
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def
+
+end
+
+class Player
+  attr_accessor :lives
+  def initialize()
+    @lives = 3
+  end
+end
+
+class Questions
+
+end
